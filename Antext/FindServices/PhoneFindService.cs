33e3716f1ce56f956d816e7332022f12634769ee
MARTINWORK\Martin <mbrabec@mediatel.cz>
2017-08-03 16:28:43 +0200

    - small improvment

--- Antext/FindServices/PhoneFindService.cs
+++ Antext/FindServices/PhoneFindService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml.Schema;
 using Antex.FindServices;
 using Antex.Objects;
 using PhoneNumbers;
@@ -9,6 +10,8 @@ namespace Antext.FindServices
 {
     public class PhoneFindService : IFindService
     {
+        private string regexPattern = "(?:(\\d{1}))(\\d| |-){7,30}(\\d)";
+
         public PhoneFindService()
         {
             
@@ -18,27 +21,36 @@ namespace Antext.FindServices
         {
             var output = new List<AntextStringItem>();
 
-            PhoneNumberUtil util = PhoneNumberUtil.GetInstance();
-
-            // This should match any sequence of numbers
-            string pattern = "(?:(\\d{1}))(\\d| |.){7,30}(\\d)";
-            Regex.Match(text, pattern);
-
-            // TODO continue here
-
-
-            string possible = "777701970";
-            var res = util.IsPossibleNumber(possible, null);
-
-            // This phone number can not be found
-
-            List<PhoneNumberMatch> result = util.FindNumbers(text, null, PhoneNumberUtil.Leniency.POSSIBLE, 100).ToList();
+            PhoneNumberUtil phoneUtils = PhoneNumberUtil.GetInstance();
+            List<PhoneNumberMatch> result = phoneUtils.FindNumbers(text, null, PhoneNumberUtil.Leniency.POSSIBLE, 100).ToList();
 
             foreach (var phoneNumberMatch in result)
             {
-                output.Add(new AntextStringItem(AntextStringItemType.PhoneNumber, phoneNumberMatch.RawString, phoneNumberMatch.Start));
+                output.Add(new AntextStringItem(AntextStringItemType.PhoneNumber, phoneNumberMatch.Start, phoneNumberMatch.RawString));
             }
-           
+
+            if (!output.Any())
+            {
+                // Nothing was found, try use regex
+                // This should match any sequence of numbers, separated by space or hypen, having 7-30 chars
+                string pattern = "(?:(\\d{1}))(\\d| |-){7,30}(\\d)";
+                var matches = Regex.Match(text, pattern);
+                if (matches.Success)
+                {
+                    foreach (Capture match in matches.Captures)
+                    {
+                        // Replace spaces or hypens if any
+                        string fix = match.Value.Replace(" ", "").Replace("-", "");
+
+                        // If fixed's string length match possible phone number length, try to fix it
+                        if (fix.Length > 8 && fix.Length < 14)
+                        {
+                            //var fixedPhoneNumber = phoneUtils.Parse(fix, "");
+                            output.Add(new AntextStringItem(AntextStringItemType.PhoneNumber, match.Index, match.Value, PhoneNumberUtil.Normalize(fix)));
+                        }
+                    }
+                }
+            }
 
             return output;
 
--- Antext/Objects/AntextStringItem.cs
+++ Antext/Objects/AntextStringItem.cs
@@ -3,14 +3,17 @@
     public class AntextStringItem
     {
         public AntextStringItemType Type { get; private set; }
-        public string Value { get; private set; }
+        public string OriginalValue { get; private set; }
         public long StartIndex { get; private set; }
+        public string FixedValue { get; private set; }
 
-        public AntextStringItem(AntextStringItemType type, string value, long startIndex)
+        public AntextStringItem(AntextStringItemType type, long startIndex, string originalValue, string fixedValue = null)
         {
             Type = type;
-            Value = value;
+            OriginalValue = originalValue;
             StartIndex = startIndex;
+
+            FixedValue = fixedValue ?? originalValue;
         }
     }
 }
